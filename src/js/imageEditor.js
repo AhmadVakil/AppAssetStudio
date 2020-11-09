da7bd16e2d0c66fac62e70b945fc86063e74f452
Ahmadreza <ahmad.vakil.safir@gmail.com>
2020-11-09 23:51:05 +0100

    upgrades-image-editor

--- src/js/imageEditor.js
+++ src/js/imageEditor.js
@@ -44,6 +44,60 @@ function crop() {
     foto.cropSelected();
 }
 
+function getTheIcon() {
+    var img=document.getElementById("foto-image");
+    var canvas=document.getElementById("can");
+
+    // Use the width for both width and height, most common icon launchers has the same width and height
+    var squareLength = img.width;
+    canvas.width = squareLength;
+    canvas.height = squareLength;
+    ctx=canvas.getContext("2d");
+
+    // Shadow & Glow
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 10;
+    ctx.shadowColor = 'black';
+    ctx.shadowBlur = 10;
+
+    // Fit my image into the canvas
+    var scale = Math.min(canvas.width / img.width, canvas.height / img.height);
+    // get the top left position of the image
+    var x = (canvas.width / 2) - (img.width / 2) * scale;
+    var y = (canvas.height / 2) - (img.height / 2) * scale;
+    ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
+
+    // Get radius from my range and apply my changes
+    var radius = document.getElementById("radius-range").value;
+    ctx.save();
+    ctx.globalCompositeOperation="destination-out";
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(10,0);
+    ctx.arcTo(0,0,0,10,radius);
+    ctx.closePath();
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(squareLength,0);
+    ctx.lineTo(90,0);
+    ctx.arcTo(squareLength,0,squareLength,10,radius);
+    ctx.closePath();
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(0,squareLength);
+    ctx.lineTo(0,90);
+    ctx.arcTo(0,squareLength,10,squareLength,radius);
+    ctx.closePath();
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(squareLength,squareLength);
+    ctx.lineTo(90,squareLength);
+    ctx.arcTo(squareLength,squareLength,squareLength,90,radius);
+    ctx.closePath();
+    ctx.fill();
+    ctx.restore();
+}
+
 function fitBorder() {
     document.getElementById("img-container").style.width = document.getElementById("foto-image").offsetWidth+"px";
     document.getElementById("img-container").style.height = document.getElementById("foto-image").offsetHeight+"px";
