772e5251f1a12a2a7422f2ee134eafc43c9dd6a8
ahmadvakil <ahmad.vakil.safir@gmail.com>
2020-12-20 12:42:19 +0100

    updates

--- src/js/iconCropper.js
+++ src/js/iconCropper.js
@@ -1,5 +1,16 @@
 var socket = io.connect('127.0.0.1:5001')
 
+$(document).ready(function(){
+    $('#formID input[type=checkbox]').attr('checked',false);
+    document.getElementById('roundCorner').disabled = true;
+    document.getElementById('dropShadow').disabled = true;
+    document.getElementById('roundCorner').style.opacity = 0.5;
+    document.getElementById('dropShadow').style.opacity = 0.5;
+    document.getElementById('dropShadowDiv').style.opacity = 0.5;
+    document.getElementById('radiusLabel').style.opacity = 0.5;
+    $('#readURL').val('');
+});
+
 socket.on('Repositories', function (data) {
   var repoInfoText = document.createElement('h4')
   var error = document.createElement('i')
@@ -10,12 +21,12 @@ socket.on('Repositories', function (data) {
   var resourceDiv = document.getElementById('resource-picker')
   if (data.length !== 0) {
     failedToLoadRepoNotification.style.display = 'none'
-    repoInfoText.innerHTML = 'Select repository'
+    repoInfoText.innerHTML = 'Available Resources'
     var repositoriesMenu = document.createElement('select')
     // repositoriesMenu.addEventListener('change', openRepo)
     repositoriesMenu.id = 'repositoriesDropDownMenu'
     var defaultOption = document.createElement('option')
-    defaultOption.innerHTML = 'Select Repository'
+    defaultOption.innerHTML = 'Select resource or repository'
     repositoriesMenu.appendChild(defaultOption)
     for (var i = 0; i < data.length; i++) {
       var tempOption = document.createElement('option')
@@ -39,6 +50,12 @@ function readURL(input) {
                 .attr('src', e.target.result);
         };
         reader.readAsDataURL(input.files[0]);
+        document.getElementById('roundCorner').disabled = false;
+        document.getElementById('dropShadow').disabled = false;
+
+    } else {
+        document.getElementById('roundCorner').disabled = true;
+        document.getElementById('dropShadow').disabled = true;
     }
 }
 
@@ -48,21 +65,19 @@ function giveRadius(range) {
 }
 
 function dropShadow(range) {
-     //document.getElementById("blah").style.borderRadius = range.value+"px";
+     document.getElementById("blah").style.borderRadius = range.value+"px";
      document.getElementById("shadowTextValue").value = range.value+"px";
 }
 
 document.getElementById('roundCorner').addEventListener("change", function(){
   if (this.checked) {
     console.log("checked")
-    document.getElementById('roundCornerDiv').style.display = "block";
     document.getElementById('roundCornerDiv').style.opacity = 1;
     document.getElementById('radiusTextValue').disabled = false;
     document.getElementById('vol').disabled = false;
 
   } else {
     console.log("unchecked")
-    document.getElementById('roundCornerDiv').style.display = "none";
     document.getElementById('radiusTextValue').value = "0px";
     document.getElementById('vol').value = 0;
     document.getElementById('radiusTextValue').disabled = true;
@@ -74,13 +89,11 @@ document.getElementById('roundCorner').addEventListener("change", function(){
 document.getElementById('dropShadow').addEventListener("change", function(){
   if (this.checked) {
     console.log("checked")
-    document.getElementById('dropShadowDiv').style.display = "block";
     document.getElementById('dropShadowDiv').style.opacity = 1;
     document.getElementById('shadowTextValue').disabled = false;
     document.getElementById('shadowVol').disabled = false;
   } else {
     console.log("unchecked")
-    document.getElementById('dropShadowDiv').style.display = "none";
     document.getElementById('shadowTextValue').value = "0px";
     document.getElementById('shadowVol').value = 0;
     document.getElementById('shadowTextValue').disabled = true;
